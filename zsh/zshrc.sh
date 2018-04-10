b60c7e81f4d48d0b82fcf75b5ab5a1792ee1a885
xloc <tj-xzx@126.com>
2018-04-10 23:25:12 +0800

    correct zshrc.sh

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -2,7 +2,7 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-  export ZSH=~/dotfiles/zsh/plugins/oh-my-zsh
+export ZSH=~/dotfiles/zsh/plugins/oh-my-zsh
 
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
@@ -95,5 +95,13 @@ source $ZSH/oh-my-zsh.sh
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
+
+
+export ZSH_PLUGIN_PATH=~/dotfiles/zsh/plugins
+# zsh-autosuggestions
+source $ZSH_PLUGIN_PATH/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# zsh-syntax-highlighting
+source $ZSH_PLUGIN_PATH/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # export GOPATH=/usr/local/gopath
